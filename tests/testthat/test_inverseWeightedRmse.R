533b8ad10c8ef8b03de7d2b2da16c2a903728e5f
Thomas Weise <tweise@hfuu.edu.cn>
2018-04-02 06:27:08 +0800

    BugFix: `par` not passed on in jacobian function

--- tests/testthat/test_inverseWeightedRmse.R
+++ tests/testthat/test_inverseWeightedRmse.R
@@ -52,6 +52,58 @@ test_that("Test inverseWeightedRmse", {
 
 
 
+
+
+test_that("Test inverse weighted rmse (2)", {
+  x <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17);
+  f <- function(x, par) { sin(par[1] + (x*par[2])) };
+  origpar <- c(2, 0.2);
+  g <- function(x) f(x, origpar);
+  gradient <- function(x, par) {z <- cos(par[2]*x+par[1]); c(z, x*z) };
+  y <- g(x);
+
+  rmse <- RegressionQualityMetric.inverseWeightedRmse(x, y);
+  validObject(rmse)
+  expect_equal(is.null(rmse@quality), FALSE)
+  expect_equal(is.null(rmse@residuals), FALSE)
+  expect_equal(is.null(rmse@jacobian), FALSE)
+  expect_equal(is.null(rmse@x), FALSE)
+  expect_identical(rmse@x, x)
+  expect_equal(is.null(rmse@y), FALSE)
+  expect_identical(rmse@y, y)
+  expect_equal(is.null(rmse@weights), FALSE)
+
+  expect_equal(rmse@quality(f, origpar), 0)
+  testpar <- c(1, 0.3);
+  residuals <- rmse@residuals(f, testpar)
+  expect_identical(is.vector(residuals), TRUE)
+  expect_identical(length(residuals), length(x))
+  for( i in 1:length(x) ) {
+    expect_equal( (y[i] - f(x[i], testpar)) / abs(y[i]), residuals[i])
+  }
+  expect_equal(rmse@quality(f, testpar), sqrt(mean(residuals^2)))
+
+  jacobian <- rmse@jacobian(gradient, testpar)
+  expect_equal(is.vector(jacobian), TRUE)
+  expect_equal(length(jacobian), length(x)*length(testpar))
+  i <- 0
+  for(j in 1:length(testpar)) {
+    for(k in 1:length(x)) {
+      i <- i + 1;
+      expect_equal(jacobian[i], -gradient(x[k], testpar)[j]/abs(y[k]))
+    }
+  }
+
+  if(require(minpack.lm)) {
+    fn <- function(par) { rmse@residuals(f, par); };
+    jac <- function(par) { rmse@jacobian(gradient, par); };
+    result <- nls.lm(par=testpar, fn=fn, jac=jac);
+    expect_equal( (result$deviance < 1e-10), TRUE)
+    expect_equal( (sqrt(mean((origpar - result$par)^2)) < 1e-10), TRUE)
+  }
+})
+
+
 test_that("Test inverseWeightedRmse", {
   x <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17);
   f <- function(x, par) { par[1] + (par[2] * x) + (par[3] * (x^2)) + (par[4] * (x^3)) };
